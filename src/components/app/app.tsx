110c3c155c7ec3bb5fd099f91762b04ef5410250
Maxtop1qq <maksimsvetlov0@gmail.com>
2026-07-05 14:48:37 +0300

    Настроил маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,33 +1,50 @@
-import { ConstructorPage } from '@pages';
-import '../../index.css';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
+
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, ProfileMenu } from '@components';
 import { Preloader } from '@ui';
+import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from '../../../src/services/store';
+import { fetchIngredients } from '../../../src/services/slices/ingredientsSlice';
 
 const App = () => {
   /** TODO: взять переменные из стора */
   const isIngredientsLoading = false;
   const ingredients = [];
   const error = null;
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchIngredients());
+  });
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      {isIngredientsLoading ? (
-        <Preloader />
-      ) : error ? (
-        <div className={`${styles.error} text text_type_main-medium pt-4`}>
-          {error}
-        </div>
-      ) : ingredients.length > 0 ? (
-        <ConstructorPage />
-      ) : (
-        <div className={`${styles.title} text text_type_main-medium pt-4`}>
-          Нет игредиентов
-        </div>
-      )}
-    </div>
+    <>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile-orders' element={<ProfileOrders />} />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
