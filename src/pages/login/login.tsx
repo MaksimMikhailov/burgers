ea630622825c088c42267fc4329c312157690f5f
Maxtop1qq <maksimsvetlov0@gmail.com>
2026-07-19 14:19:57 +0300

    Регистрация

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,22 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { fetchLogin } from '../../services/slices/authSlice';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const { error } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    dispatch(fetchLogin({ email, password }));
   };
 
   return (
     <LoginUI
-      errorText=''
+      errorText={error || ''}
       email={email}
       setEmail={setEmail}
       password={password}
--- src/services/slices/authSlice.ts
+++ src/services/slices/authSlice.ts
@@ -1,11 +1,69 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { loginUserApi, registerUserApi, TLoginData, TRegisterData } from '@api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { setCookie } from '../../utils/cookie';
+import { TUser } from '@utils-types';
 interface IInitialState {
   isAuth: boolean;
+  loading: boolean;
+  error: string | null;
+  user: TUser | null;
 }
-const initialState: IInitialState = { isAuth: false };
+const initialState: IInitialState = {
+  isAuth: false,
+  loading: false,
+  error: null,
+  user: null
+};
+export const fetchRegister = createAsyncThunk(
+  'auth/fetchRegister',
+  async (data: TRegisterData) => {
+    const response = await registerUserApi(data);
+    setCookie('accessToken', response.accessToken);
+    localStorage.setItem('refreshToken', response.refreshToken);
+    return response.user;
+  }
+);
+export const fetchLogin = createAsyncThunk(
+  'auth/fetchLogin',
+  async (data: TLoginData) => {
+    const response = await loginUserApi(data);
+    setCookie('accessToken', response.accessToken);
+    localStorage.setItem('refreshToken', response.refreshToken);
+    return response.user;
+  }
+);
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRegister.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchRegister.fulfilled, (state, action) => {
+        state.isAuth = true;
+        state.user = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchRegister.rejected, (state, action) => {
+        state.error = action.error.message || 'Не удалось зарегистрироваться';
+        state.loading = false;
+      })
+      .addCase(fetchLogin.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchLogin.fulfilled, (state, action) => {
+        state.isAuth = true;
+        state.user = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchLogin.rejected, (state, action) => {
+        state.error = action.error.message || 'Не удалось авторизоваться';
+        state.loading = false;
+      });
+  }
 });
 export default authSlice.reducer;
